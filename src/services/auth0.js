d22b3560d7454321366c393aad06a588b0e8e449
Scott Stephens <zahi.stephens@gmail.com>
2018-01-07 15:39:27 -0800

    fix for app relocation

--- src/services/auth0.js
+++ src/services/auth0.js
@@ -3,7 +3,7 @@ import * as auth0 from 'auth0-js'
 export default new auth0.WebAuth({
   domain: 'ariftek.auth0.com',
   clientID: 'NjYo5Wd169t0G4IPLspKBgQtjJif1cd0',
-  redirectUri: 'http://localhost:8080/callback',
+  redirectUri: window.location.href + 'callback',
   audience: 'https://ariftek.auth0.com/userinfo',
   responseType: 'token id_token',
   scope: 'openid email'
